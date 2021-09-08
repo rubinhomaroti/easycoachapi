4f7dae037b495d795ae5653c1060510cfd7550c8
Rubens Maroti F <rubens@mobilemed.com.br>
2021-09-08 20:14:26 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/easycoachapi/EasyCoachApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easycoachapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCoachApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCoachApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/easycoachapi/EasyCoachApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easycoachapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCoachApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
