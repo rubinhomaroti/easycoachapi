d9640e6043878c98ca1be00d5350379a543161df
gabrielFragata <gfragata97@gmail.com>
2021-10-14 22:37:19 -0300

    Ajustando problema de CORS

--- src/main/java/br/com/fiap/easycoachapi/controller/UserController.java
+++ src/main/java/br/com/fiap/easycoachapi/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     private UserService userService;
 
